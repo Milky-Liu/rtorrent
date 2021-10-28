edb001f70775e899c18692d25340299ac3e827a3
milky <359682481@qq.com>
2021-10-28 12:06:35 +0800

    Update rt_ru_install.sh

--- rt_ru_install.sh
+++ rt_ru_install.sh
@@ -189,7 +189,8 @@ echo -e "${plain}============================================================"
 cd /tmp
 rm ruTorrent-3.7.zip -f
 rm ruTorrent-master -rf
-wget -O ruTorrent-3.7.zip https://bintray.com/novik65/generic/download_file?file_path=ruTorrent-3.7.zip
+#wget -O ruTorrent-3.7.zip https://bintray.com/novik65/generic/download_file?file_path=ruTorrent-3.7.zip
+wget -O ruTorrent-3.7.zip https://github.com/Novik/ruTorrent/archive/refs/tags/v3.8.zip
 unzip -q ruTorrent-3.7.zip
 rm ruTorrent-master/.htaccess -f
 mv ruTorrent-master ${webroot}/rutorrent
